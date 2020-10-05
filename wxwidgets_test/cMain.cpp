c28f70b9e7a5a04672211432bf568f3ccbd57f66
FiendChain <formulating.response@gmail.com>
2020-10-06 09:41:43 +1100

    Use relative filepath for model

--- wxwidgets_test/cMain.cpp
+++ wxwidgets_test/cMain.cpp
@@ -1,15 +1,23 @@
 #include "cMain.h"
-#include <iostream>
+
+#include <string>
+#include <filesystem>
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-    EVT_CLOSE(cMain::onClose)
+EVT_CLOSE(cMain::onClose)
 wxEND_EVENT_TABLE()
+
+namespace fs = std::filesystem;
 
 cMain::cMain()
 : wxFrame(nullptr, wxID_ANY, "Test title")
 {
-    const char* DEFAULT_FILEPATH = "C:/Users/acidi/Coding/Projects/SoccerBot/assets/models/cnn_113_80_quantized.tflite";
-    m_model = std::make_shared<Model>(DEFAULT_FILEPATH);
+    const std::string filepath_str = "./cnn_113_80_quantized.tflite";
+    const fs::path filepath = filepath_str;
+    const fs::path abs_filepath = fs::absolute(filepath);
+
+    m_model = std::make_shared<Model>(abs_filepath.string().c_str());
+
     m_model->show_info();
 
     m_screenshot_timer = std::make_unique<ScreenshotTimer>(this, m_model);
